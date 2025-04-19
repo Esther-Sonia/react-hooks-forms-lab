f4a5b6b8d3a9b9e528df6b947278815e77050ab4
Esther-Sonia <esthersonia21@gmail.com>
2025-04-19 23:52:24 +0300

    implement shopping list with filter and itemform

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -3,23 +3,30 @@ import ItemForm from "./ItemForm";
 import Filter from "./Filter";
 import Item from "./Item";
 
-function ShoppingList({ items }) {
+function ShoppingList({ items: initialItems }) {
+  const [items, setItems] = useState(initialItems);
   const [selectedCategory, setSelectedCategory] = useState("All");
+  const [searchText, setSearchText] = useState("");
 
   function handleCategoryChange(event) {
     setSelectedCategory(event.target.value);
   }
 
-  const itemsToDisplay = items.filter((item) => {
-    if (selectedCategory === "All") return true;
+  function handleItemFormSubmit(newItem) {
+    setItems([...items, newItem]);
+  }
 
-    return item.category === selectedCategory;
+  const itemsToDisplay = items.filter((item) => {
+  const categoryMatch = selectedCategory === "All" || 
+  item.category === selectedCategory;
+
+    return item.name.toLowerCase().includes(searchText.toLowerCase());
   });
 
   return (
     <div className="ShoppingList">
-      <ItemForm />
-      <Filter onCategoryChange={handleCategoryChange} />
+      <ItemForm onItemFormSubmit={handleItemFormSubmit} />
+      <Filter onCategoryChange={handleCategoryChange} onSearchChange={setSearchText} search={searchText} />
       <ul className="Items">
         {itemsToDisplay.map((item) => (
           <Item key={item.id} name={item.name} category={item.category} />
